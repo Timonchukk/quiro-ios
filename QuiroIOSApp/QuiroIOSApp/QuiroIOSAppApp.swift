94665051a50b3466a062f73bf66827277eadb7a6
timonchukdesign <timonchukdesign@Mac-mini-103.local>
2026-03-08 16:31:21 -0700

    Fix widget Info.plist path

---
+++ QuiroIOSApp/QuiroIOSApp/QuiroIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuiroIOSAppApp.swift
+//  QuiroIOSApp
+//
+//  Created by timonchukdesign on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct QuiroIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
